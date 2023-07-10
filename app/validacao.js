104083cc5db295062a28cc6311bcf342694c5607
vanessalaureano <solytagamer@gmail.com>
2023-07-09 23:48:18 -0300

    update

---
+++ app/validacao.js
@@ -0,0 +1,36 @@
+function verificaSeChuteValorValido(chute) {
+    const numero = +chute;
+
+    if(chuteInvalido(numero)) {
+        elementoChute.innerHTML +='<div>Valor inválido.</div>';
+        return;
+    }
+
+    if (numeroMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML +=`<div>Valor inválido. Fale um número entre ${menorValor} e ${maiorValor}.</div>`;
+        return;
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Parabéns, você acertou!</h2>
+            <h3>O número secreto é ${numeroSecreto}.</h3>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
